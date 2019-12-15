1fdad2627463ac0ce4d3ece9afc19d977005afa7
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2019-12-15 12:31:33 -0800

    04-02

--- ECommerce.Api.Products.Tests/ProductsServiceTest.cs
+++ ECommerce.Api.Products.Tests/ProductsServiceTest.cs
@@ -33,6 +33,49 @@ namespace ECommerce.Api.Products.Tests
             Assert.Null(product.ErrorMessage);
         }
 
+        [Fact]
+        public async Task GetProductReturnsProductUsingValidId()
+        {
+            var options = new DbContextOptionsBuilder<ProductsDbContext>()
+                .UseInMemoryDatabase(nameof(GetProductReturnsProductUsingValidId))
+                .Options;
+            var dbContext = new ProductsDbContext(options);
+            CreateProducts(dbContext);
+
+            var productProfile = new ProductProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
+            var mapper = new Mapper(configuration);
+
+            var productsProvider = new ProductsProvider(dbContext, null, mapper);
+
+            var product = await productsProvider.GetProductAsync(1);
+            Assert.True(product.IsSuccess);
+            Assert.NotNull(product.Product);
+            Assert.True(product.Product.Id == 1);
+            Assert.Null(product.ErrorMessage);
+        }
+
+        [Fact]
+        public async Task GetProductReturnsProductUsingInvalidId()
+        {
+            var options = new DbContextOptionsBuilder<ProductsDbContext>()
+                .UseInMemoryDatabase(nameof(GetProductReturnsProductUsingInvalidId))
+                .Options;
+            var dbContext = new ProductsDbContext(options);
+            CreateProducts(dbContext);
+
+            var productProfile = new ProductProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
+            var mapper = new Mapper(configuration);
+
+            var productsProvider = new ProductsProvider(dbContext, null, mapper);
+
+            var product = await productsProvider.GetProductAsync(-1);
+            Assert.False(product.IsSuccess);
+            Assert.Null(product.Product);
+            Assert.NotNull(product.ErrorMessage);
+        }
+
         private void CreateProducts(ProductsDbContext dbContext)
         {
             for (int i = 1; i <= 10; i++)
